ccaa92354ed24ee895926d812334e4b632fdff6c
WhatIsCW <124468263+DatMxkey@users.noreply.github.com>
2024-05-18 13:43:21 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DatMxkey/Nexware/main/arsenal.lua"))() -- This is only temp untill the loader is finished.
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DatMxkey/Nexware/main/arsenal.lua"))()
+end
+
+
+if game.PlaceId == 8737899170 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DatMxkey/Nexware/main/ps99.lua"))()
+end
